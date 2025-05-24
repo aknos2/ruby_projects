b324cc4a3726e48d05b0fdd0e9fff0a79382560d
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2025-05-24 12:19:04 +0900

    Create caesar_cipher method

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,1 +1,14 @@
-puts "hello thre"
+def caesar_cipher(string, shift_value = 3)
+  unless string.match?(/\A[a-zA-Z]+\z/)
+    return "You must write a word with only letters"
+  end
+
+  string.chars.map do |char|
+    base = char == char.upcase ? 'A'.ord : 'a'.ord
+    (((char.ord - base + shift_value ) % 26) + base).chr
+  end.join
+end
+
+puts "Input word:"
+input = gets.chomp
+puts caesar_cipher(input)
